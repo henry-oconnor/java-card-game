413d22004d9eb05a04aa370c1a75019c64e465f3
Henry O'Connor <hen.oconnor@gmail.com>
2017-10-06 16:18:02 -0400

    Added basic chip and bank class

--- src/groupprojectcardgame/PlayerBank.java
+++ src/groupprojectcardgame/PlayerBank.java
@@ -3,6 +3,7 @@
  */
 package groupprojectcardgame;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 /**
@@ -10,21 +11,24 @@ import java.util.Stack;
  * @author henoc
  * A player's bank for betting and buy-ins
  */
-public class Bank {
-    int total;
+public class PlayerBank {
+    private Integer total;
     private Stack<Chip> whiteChips;
     private Stack<Chip> redChips;
     private Stack<Chip> blueChips;
     private Stack<Chip> greenChips;
     private Stack<Chip> blackChips;
+    
+    private HashMap<Chip.Color,Stack<Chip>> bank;
             
     // buyin should be in multiples of 100
     // creates a reasonable chip distribution from buyin amount
-    public Bank(int buyin){
+    public PlayerBank(int buyin){
         double remaining = buyin;
         total = buyin;
         if(buyin > 500){
             blackChips = new Stack<>();
+            bank.put(Chip.Color.BLACK, blackChips);
             while(remaining / buyin > .8){
                 blackChips.push(new Chip(Chip.Color.BLACK));
                 remaining -= 100;
@@ -32,25 +36,30 @@ public class Bank {
         }
         if(buyin > 100){
             greenChips = new Stack<>();
+            bank.put(Chip.Color.GREEN, greenChips);
             while(remaining / buyin > .5){
                 greenChips.push(new Chip(Chip.Color.GREEN));
                 remaining -= 25;
             }
                 
         }
+        
         blueChips = new Stack<>();
+        bank.put(Chip.Color.BLUE, blueChips);
         while(remaining / buyin > .25){
             blueChips.push(new Chip(Chip.Color.BLUE));
             remaining -= 10;
         }
-                
+        
         redChips = new Stack<>();
+        bank.put(Chip.Color.RED, redChips);
         while(remaining / buyin > .1){
             redChips.push(new Chip(Chip.Color.RED));
             remaining -= 5;
         }
         
         whiteChips = new Stack<>();
+        bank.put(Chip.Color.WHITE, whiteChips);
         while(remaining > 0){
             whiteChips.push(new Chip(Chip.Color.WHITE));
             remaining -= 1;
@@ -59,9 +68,38 @@ public class Bank {
             
     }
     
+    /**
+     * Takes a stack of chips and sorts them onto the correct player stacks
+     * 
+     * @param winnings 
+     */
+    public void takeWinnings(Stack<Chip> winnings){
+        Chip temp;
+        while(!winnings.empty()){
+            temp = winnings.pop();
+            total += temp.color.getValue();
+            bank.get(temp.color).push(temp);
+        }
+    }
     
-    // takeWinnings()
+    /**
+     * 
+     * I'm thinking the user will click a chip or chip button, which will call this
+     * function with the correct color and pop from the correct stack
+     * 
+     * @param c 
+     * @return Chip
+     */
+    public Chip popChip(Chip.Color c){
+        total -= c.getValue();
+        return bank.get(c).pop();
+    }
     
-            
+    /**
+     * Returns a stack of chips of equal value to the input amount
+     * 
+     * @param amount
+     * @return 
+     */    
     
 }
